ee99ffa1f264f9d77d61b8b7fc208e4c934b7972
백지현 <racheal1227@gmail.com>
2022-10-07 17:04:21 +0900

    #6.0-6.5

---
+++ test.js
@@ -0,0 +1,11 @@
+function asdf() {
+  const test = () => {
+    //sdklfjasdl;kfja;slkd
+    const a = 1;
+  };
+  const tsd = () => {
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+    }
+  };
+}
